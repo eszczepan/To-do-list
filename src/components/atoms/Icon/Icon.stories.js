bb5f2d17e139de6a4473bba41b7428cc646d350d
Szczepan <s.szablej1@gmail.com>
2020-08-18 01:21:58 +0200

    :art: Correct storybook views

--- src/components/atoms/Icon/Icon.stories.js
+++ src/components/atoms/Icon/Icon.stories.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import dragIcon from 'assets/dragIcon.svg';
 import removeIcon from 'assets/remove.svg';
+import toDoIcon from 'assets/list.svg';
+import inProgressIcon from 'assets/timer.svg';
+import doneIcon from 'assets/tick.svg';
 import Icon from './Icon';
 
 export default {
@@ -10,3 +13,6 @@ export default {
 
 export const Drag = () => <Icon icon={dragIcon} />;
 export const Remove = () => <Icon icon={removeIcon} />;
+export const ToDo = () => <Icon icon={toDoIcon} />;
+export const InProgress = () => <Icon icon={inProgressIcon} />;
+export const Done = () => <Icon icon={doneIcon} />;
--- src/components/atoms/Input/Input.stories.js
+++ src/components/atoms/Input/Input.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import Input from './Input';
 
 export default {
@@ -6,6 +7,19 @@ export default {
   title: 'Input',
 };
 
-export const Normal = () => <Input />;
-export const Title = () => <Input title />;
-export const Description = () => <Input description />;
+const StyledWrapper = styled.div`
+  background-color: ${({ theme }) => theme.white};
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Title = () => (
+  <StyledWrapper>
+    <Input title placeholder="Wpisz tytuł..." />
+  </StyledWrapper>
+);
+export const Description = () => (
+  <StyledWrapper>
+    <Input description placeholder="Tutaj wpisz opis..." />
+  </StyledWrapper>
+);
